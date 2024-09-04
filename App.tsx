aad3aff073968806f0d3cc0d8d2c4b53e4006f67
Mac <mac@Macs-MacBook-Pro.local>
2024-09-04 14:47:06 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
-import Root from './src/navigation/root';
+import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
 
 function App(): JSX.Element {
-  return <Root />;
+  
+
+  const [randomBg, setRandombg] = useState('#ffffff');
+  const generateColor = () => {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * hex.length)];
+      }
+      setRandombg(color)
+  }
+
+  return (
+  <>
+    <StatusBar backgroundColor={randomBg} />
+    <View style={[styles.container, {backgroundColor: randomBg}]}>
+      <TouchableOpacity onPress={ () => generateColor()}>
+        <View style={styles.actionBtn}>
+          <Text style={styles.actionBtnTxt}> Press Me </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  </>
+  )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  actionBtn: {
+    borderRadius: 10,
+    backgroundColor: '#6A1B',
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+  },
+  actionBtnTxt: {
+    fontSize: 24,
+    color: '#ffffff',
+    textTransform: 'uppercase'
+  }
+});
+ 
 export default App;
